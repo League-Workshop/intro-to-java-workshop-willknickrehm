a54dc8b369668dfb2bdbd6eb94b1755784f6da40
willknickrehm <league@WTS-iMac-11.attlocal.net>
2018-04-12 14:51:41 -0700

    day4
    fortuneteller

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,41 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+	public static void main(String[] args) {
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
+		int randumnumber = new Random().nextInt(4);
+		// 3. Print out this variable
+		System.out.println(randumnumber);
+		// 4. Get the user to enter something that they think is awesome
+		String response = JOptionPane.showInputDialog(null, "type something awsome");
+		// 5. If the random number is 0
+		if (randumnumber == 0) {
+			// -- tell the user whatever they entered is awesome!
+			JOptionPane.showMessageDialog(null, "that is awsome");
+			// 6. If the random number is 1
+		}
+		if (randumnumber == 1) {
+			// -- tell the user whatever they entered is ok.
+			JOptionPane.showMessageDialog(null, "that's ok");
+			// 7. If the random number is 2
+		}
+		if (randumnumber == 2) {
+			// -- tell the user whatever they entered is boring.
+			JOptionPane.showMessageDialog(null, "that's boring");
+			// 8. If the random number is 3
+		}
+		if (randumnumber == 3) {
+			// -- write your own answer
+			JOptionPane.showMessageDialog(null, "your dumb");
+		}
 
-	// 3. Print out this variable
-
-	// 4. Get the user to enter something that they think is awesome
-
-	// 5. If the random number is 0
-
-	// -- tell the user whatever they entered is awesome!
-
-	// 6. If the random number is 1
-
-	// -- tell the user whatever they entered is ok.
-
-	// 7. If the random number is 2
-
-	// -- tell the user whatever they entered is boring.
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
+	}
 
 }
-
-
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,34 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String two = JOptionPane.showInputDialog(null, "what's 1+1?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(two.equals("two")) {
 		// 4.  if the user's answer is correct
-		
+		//this code runs if
+			//two is equal to two
 		// -- add one to their score 
-		
+		score = score + 1;
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String twohundred = JOptionPane.showInputDialog(null, "what's 100 + 100?");	
+		if(twohundred.equals("twohundred")) {
+			//this code runs if
+			//twohundred is equal to twohundred
+			score = score + 1;
+		}
+
 		// 6.  After all the questions have been asked, print the user's score 
-		
-	}
+		JOptionPane.showMessageDialog(null, " score " + score);
+
+}
+
 }
